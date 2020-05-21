6321944abf493b30c4bbba11d63bfeb05fdb5311
prislash21 <prislash21@gmail.com>
2020-05-21 23:47:06 +0600

    PROJECT LAYOUT UPDATED WITH DASHBOARD AND DYNAMIC BOOKDETAILS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavBar from "./components/layout/NavBar";
+import Dashboard from "./components/dashboard/Dashboard";
+import BookDetails from "./components/Books/BookDetails"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route  exact path='/' component={Dashboard} />
+          <Route  exact path='/bookdetails/:id' component={BookDetails} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
